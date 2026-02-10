b95bcce3b680cdb097124cafc18c3f23ee33dc6c
Kavixx12 <kaveeshapathumina1234@gmail.com>
2026-02-10 22:38:01 +0530

    Created StudentController with POST method

---
+++ src/main/java/com/kaveesha/student_management/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.kaveesha.student_management.controller;
+
+import com.kaveesha.student_management.entity.Student;
+import com.kaveesha.student_management.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+}
